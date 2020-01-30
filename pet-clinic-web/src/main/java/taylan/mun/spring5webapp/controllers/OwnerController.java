c997b2378f94dd47b4b1025f46fbee9e6d6cb65a
taylanmun <tmun@maritimetrainer>
2020-01-30 12:43:54 +0300

    Adding Owners index page and controller. Closes #13

---
+++ pet-clinic-web/src/main/java/taylan/mun/spring5webapp/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package taylan.mun.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"","/", "/index", "/index.html"})
+    public String listOwners(){
+        return "/owners/index";
+    }
+}
