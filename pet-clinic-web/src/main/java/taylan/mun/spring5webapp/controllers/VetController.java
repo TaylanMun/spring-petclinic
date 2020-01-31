a711c2004f42c2324f8bda66b7a236421cc4e142
taylanmun <tmun@maritimetrainer>
2020-01-31 13:38:15 +0300

    added Vets List and updated controller. Closes #15

--- pet-clinic-web/src/main/java/taylan/mun/spring5webapp/controllers/VetController.java
+++ pet-clinic-web/src/main/java/taylan/mun/spring5webapp/controllers/VetController.java
@@ -1,13 +1,23 @@
 package taylan.mun.spring5webapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import taylan.mun.spring5webapp.services.VetService;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String indexVets(){
+    public String indexVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
