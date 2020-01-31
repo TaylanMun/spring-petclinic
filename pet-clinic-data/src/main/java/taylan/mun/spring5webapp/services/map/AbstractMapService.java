ec1741183b94ff678ce341bd25f436fd155db00a
taylanmun <tmun@maritimetrainer>
2020-01-31 14:24:53 +0300

    Refactored Map Imp to abstract ID generation Closes #23

--- pet-clinic-data/src/main/java/taylan/mun/spring5webapp/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/taylan/mun/spring5webapp/services/map/AbstractMapService.java
@@ -1,15 +1,14 @@
 package taylan.mun.spring5webapp.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import taylan.mun.spring5webapp.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-    Set<T> findAll(){
+    protected Map<Long, T> map = new HashMap<>();
+
+    Set<T> findAll() {
         return new HashSet<>(map.values());
     }
 
@@ -17,17 +16,39 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object){
-        map.put(id, object);
+    T save(T object) {
+
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
 
         return object;
     }
 
-    void deletedById(ID id){
+    void deletedById(ID id) {
         map.remove(id);
     }
 
-    void delete(T object){
+    void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+
+        Long nextID = null;
+
+        try {
+            nextID = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nextID = 1L;
+        }
+
+        return nextID;
     }
 }
