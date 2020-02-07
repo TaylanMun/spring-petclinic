ede4d89cdb60da9260ff3a69c05ca62c6a8dea86
taylanmun <tmun@maritimetrainer>
2020-02-07 11:15:08 +0300

    added specialities, added to vet, closes #27

--- pet-clinic-data/src/main/java/taylan/mun/spring5webapp/model/Vet.java
+++ pet-clinic-data/src/main/java/taylan/mun/spring5webapp/model/Vet.java
@@ -2,5 +2,17 @@ package taylan.mun.spring5webapp.model;
 
 import taylan.mun.spring5webapp.model.Person;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
