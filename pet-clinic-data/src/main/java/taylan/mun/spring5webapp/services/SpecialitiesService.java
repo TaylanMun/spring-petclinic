e0b6d9485c73ffcb8d7752125264f121e6e3ac4b
taylanmun <tmun@maritimetrainer>
2020-02-07 14:05:12 +0300

    Added Specialities Map Impl, closes #31

---
+++ pet-clinic-data/src/main/java/taylan/mun/spring5webapp/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package taylan.mun.spring5webapp.services;
+
+import taylan.mun.spring5webapp.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/taylan/mun/spring5webapp/services/map/SpecialityServiceMap.java
@@ -0,0 +1,35 @@
+package taylan.mun.spring5webapp.services.map;
+
+import org.springframework.stereotype.Service;
+import taylan.mun.spring5webapp.model.Speciality;
+import taylan.mun.spring5webapp.services.SpecialitiesService;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deletedById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
