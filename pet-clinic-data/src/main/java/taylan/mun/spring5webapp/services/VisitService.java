00ec2df05e2d40e8d338a6d2156d38504e9686f6
Taylan Mun <tmun@maritimetrainer.com>
2020-02-17 21:30:54 +0300

    Implemented Map impl for Visit Service , closes #46

---
+++ pet-clinic-data/src/main/java/taylan/mun/spring5webapp/services/VisitService.java
@@ -0,0 +1,6 @@
+package taylan.mun.spring5webapp.services;
+
+import taylan.mun.spring5webapp.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/taylan/mun/spring5webapp/services/map/VisitMapService.java
@@ -0,0 +1,44 @@
+package taylan.mun.spring5webapp.services.map;
+
+import org.springframework.stereotype.Service;
+import taylan.mun.spring5webapp.model.Visit;
+import taylan.mun.spring5webapp.services.CrudService;
+import taylan.mun.spring5webapp.services.VisitService;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null
+                || visit.getPet().getOwner() == null
+                || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deletedById(id);
+    }
+}
