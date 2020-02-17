7742766299710ca936bb2899cdb3993b3e6bcf04
taylanmun <tmun@maritimetrainer>
2020-02-17 16:56:10 +0300

    Updated base entity with JPA annotions. Closes #35

--- pet-clinic-data/src/main/java/taylan/mun/spring5webapp/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/taylan/mun/spring5webapp/model/BaseEntity.java
@@ -1,8 +1,16 @@
 package taylan.mun.spring5webapp.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
 
     public Long getId() {
