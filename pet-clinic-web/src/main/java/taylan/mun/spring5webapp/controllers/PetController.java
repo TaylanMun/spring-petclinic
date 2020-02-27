6488a5f496fc215dbba4f4be1c3a246ff4af605b
taylanmun <tmun@maritimetrainer>
2020-02-27 11:41:46 +0300

    Adding model attributes for Pet Controller, #56

---
+++ pet-clinic-web/src/main/java/taylan/mun/spring5webapp/controllers/PetController.java
@@ -0,0 +1,46 @@
+package taylan.mun.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import taylan.mun.spring5webapp.model.Owner;
+import taylan.mun.spring5webapp.model.PetType;
+import taylan.mun.spring5webapp.services.OwnerService;
+import taylan.mun.spring5webapp.services.PetService;
+import taylan.mun.spring5webapp.services.PetTypeService;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+
+    private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> popularPetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerbinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+}
