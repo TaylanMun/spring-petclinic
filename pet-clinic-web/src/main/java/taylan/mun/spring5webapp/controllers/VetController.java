d94f431683ba2f99eed6c80271f50b23def8ddc4
Taylan Mun <tmun@maritimetrainer.com>
2020-03-08 21:37:32 +0300

    added json endpoint. closes #68

--- pet-clinic-web/src/main/java/taylan/mun/spring5webapp/controllers/VetController.java
+++ pet-clinic-web/src/main/java/taylan/mun/spring5webapp/controllers/VetController.java
@@ -2,8 +2,13 @@ package taylan.mun.spring5webapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import taylan.mun.spring5webapp.model.Vet;
 import taylan.mun.spring5webapp.services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -20,5 +25,11 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+//    @ResponseBody return JSON
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
